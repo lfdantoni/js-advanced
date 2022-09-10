5145c331710554e2afdc95d4dc30cc15b51f4ac6
lfdantoni <lfdantoni@gmail.com>
2022-09-10 13:29:01 -0300

    add lesson6 examples

---
+++ lesson6/dragdrop.js
@@ -0,0 +1,44 @@
+const drag = document.getElementById('drag')
+const file = document.getElementById('my-file')
+
+const filesToProcess = files => {
+  console.log(files)
+
+  // See formdata example first / ver el ejemplo formdata primero 
+  const data = new FormData()
+  
+  for (let index = 0; index < files.length; index++) {
+    const file = files[index];
+    data.append(`image-${index}`, file)
+  }
+
+  const xhr = new XMLHttpRequest()
+  // xhr.setRequestHeader('content-type', '?')
+  xhr.open('post', 'url')
+  xhr.send(data)
+}
+
+file.addEventListener('change', () => {
+  filesToProcess(file.files)
+})
+
+drag.addEventListener('dragenter', e => {
+  e.preventDefault()
+  console.log('Drag enter')
+})
+
+drag.addEventListener('dragleave', e => {
+  e.preventDefault()
+  console.log('Drag leave')
+})
+
+drag.addEventListener('dragover', e => {
+  e.preventDefault()
+  console.log('Drag over')
+})
+
+drag.addEventListener('drop', e => {
+  e.preventDefault()
+  console.log('Drop')
+  filesToProcess(e.dataTransfer.files)
+})
