379e06afb69c7ed6f52c7b20a955ac4a31519a3c
lfdantoni <lfdantoni@gmail.com>
2023-05-02 21:27:11 -0300

    clean repo

--- lesson7/geolocation.js
+++
@@ -1,9 +0,0 @@
-navigator.geolocation.getCurrentPosition(function(position) {
-  console.log(position.coords.latitude, position.coords.longitude, position.coords.accuracy);
-  console.log(`https://www.google.com/maps/search/?api=1&query=${position.coords.latitude},${position.coords.longitude}`)
-});
-
-navigator.geolocation.watchPosition(function(position) {
-  console.log(position.coords.latitude, position.coords.longitude, position.coords.accuracy);
-  console.log(`https://www.google.com/maps/search/?api=1&query=${position.coords.latitude},${position.coords.longitude}`)
-});
